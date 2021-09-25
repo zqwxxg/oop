4dfef76aff530ed3acd20a44f90546f9707a5241
Zi Qi <zwon0015@student.monash.edu>
2021-09-25 20:56:04 +0800

    modified rest action under reset instance

--- src/game/Player.java
+++ src/game/Player.java
@@ -13,12 +13,35 @@ import java.util.List;
 
 /**
  * Class representing the Player.
+ * @see edu.monash.fit2099.engine
+ * @see Abilities
+ * @see Status
+ * @see Resettable
+ * @see Soul
+ * @see EstusFlask
+ * @see Token
+ * @see Broadsword
  */
 public class Player extends Actor implements Soul, Resettable {
 
+	/**
+	 * The menu that is displayed to the user
+	 */
 	private final Menu menu = new Menu();
+
+	/**
+	 * The location of the last bonfire that the player rested on
+	 */
 	private Location lastBonfire;
+
+	/**
+	 * The count of souls
+	 */
 	private int soulCount;
+
+	/**
+	 * The location of the last token
+	 */
 	private Location lastToken;
 
 	/**
@@ -40,15 +63,17 @@ public class Player extends Actor implements Soul, Resettable {
 		inventory.add(new Broadsword());
 	}
 
+	/**
+	 * Setter for lastBonfire attribute
+	 *
+	 * @param lastBonfire the location of the bonfire that the player rested on
+	 */
 	public void setLastBonfire(Location lastBonfire) {
 		this.lastBonfire = lastBonfire;
 	}
 
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
-		// initialize location of bonfire
-		lastBonfire = map.at(38, 11);
-
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
@@ -61,7 +86,6 @@ public class Player extends Actor implements Soul, Resettable {
 
 	@Override
 	public void transferSouls(Soul soulObject) {
-		//TODO: transfer Player's souls to another Soul's instance.
 		soulObject.addSouls(soulCount);
 		soulCount = 0;
 	}
@@ -79,7 +103,7 @@ public class Player extends Actor implements Soul, Resettable {
 
 	@Override
 	public boolean subtractSouls(int souls) {
-		if (soulCount >= souls) {
+		if (soulCount >= souls && souls >= 0) {
 			soulCount -= souls;
 			return true;
 		}
@@ -98,53 +122,92 @@ public class Player extends Actor implements Soul, Resettable {
 			}
 		}
 
-		// modify the location according to the direction given to the latest bonfire's location or dying place at valley
 		Location location = map.locationOf(this);
 		int x = location.x();
 		int y = location.y();
 
-		if (direction != null) {
-			switch (direction) {
-				// to place soul at south of valley
-				case "North" -> y += 1;
-				// to place soul at south-west of valley
-				case "North-East" -> {
-					x -= 1;
-					y += 1;
-				}
-				// to place soul at west of valley
-				case "East" -> x -= 1;
-				// to place soul at north-west of valley
-				case "South-East" -> {
-					x -= 1;
-					y -= 1;
-				}
-				// to place soul at north of valley
-				case "South" -> y -= 1;
-				// to place soul at north-east of valley
-				case "South-West" -> {
-					x += 1;
-					y -= 1;
-				}
-				// to place soul at east of valley
-				case "West" -> x += 1;
-				// to place soul at south-east of valley
-				case "North-West" -> {
-					x += 1;
-					y += 1;
-				}
-			}
-			location = map.at(x, y);
-		}
-
-
 		switch (status) {
-			case REST -> lastBonfire = location;
+			case REST -> {
+				// modify the location according to the direction given to the latest bonfire's location
+				// if direction is not null, the player rested at the adjacent square of bonfire
+				if (direction != null) {
+					switch (direction) {
+						// bonfire is at north of the location that player rested on
+						case "North" -> y -= 1;
+						// bonfire is at north-east of the location that player rested on
+						case "North-East" -> {
+							x += 1;
+							y -= 1;
+						}
+						// bonfire is at east of the location that player rested on
+						case "East" -> x += 1;
+						// bonfire is at south-east of the location that player rested on
+						case "South-East" -> {
+							x += 1;
+							y += 1;
+						}
+						// bonfire is at south of the location that player rested on
+						case "South" -> y += 1;
+						// bonfire is at south-west of the location that player rested on
+						case "South-West" -> {
+							x -= 1;
+							y += 1;
+						}
+						// bonfire is at west of the location that player rested on
+						case "West" -> x += 1;
+						// bonfire is at north-west of the location that player rested on
+						case "North-West" -> {
+							x -= 1;
+							y -= 1;
+						}
+					}
+					location = map.at(x, y);
+				}
+				lastBonfire = location;
+			}
 			case SOFT_RESET -> {
+				// modify the location according to the dying place at valley
+				// if direction is not null, the player died from falling to valley,
+				// else, the player is killed by enemies
+				if (direction != null) {
+					switch (direction) {
+						// to place soul at south of valley
+						case "North" -> y += 1;
+						// to place soul at south-west of valley
+						case "North-East" -> {
+							x -= 1;
+							y += 1;
+						}
+						// to place soul at west of valley
+						case "East" -> x -= 1;
+						// to place soul at north-west of valley
+						case "South-East" -> {
+							x -= 1;
+							y -= 1;
+						}
+						// to place soul at north of valley
+						case "South" -> y -= 1;
+						// to place soul at north-east of valley
+						case "South-West" -> {
+							x += 1;
+							y -= 1;
+						}
+						// to place soul at east of valley
+						case "West" -> x += 1;
+						// to place soul at south-east of valley
+						case "North-West" -> {
+							x += 1;
+							y += 1;
+						}
+					}
+					location = map.at(x, y);
+				}
 				Token token = Token.getInstance("Token of Souls", '$');
 				transferSouls(token);
 				location.addItem(token);
+				// if the location of last token is not null, which means token has been placed on map,
 				if (lastToken != null) {
+					// remove it from the map
 					lastToken.removeItem(token);
 				}
 				lastToken = location;
@@ -194,7 +257,6 @@ public class Player extends Actor implements Soul, Resettable {
 			return "Drink from Estus Flask " + est.getChargeCount() + " charges remaining";
 		}
 	}
-
 
 	public int getMaxHitPoints(){
 		return maxHitPoints;
